62290c0ea168ae78a1ae20ec4eb11f92764783ab
Kemran <45750128+cat-ai@users.noreply.github.com>
2021-02-20 17:09:25 +0300

    Update errors.scala

--- src/main/scala/zio/windows/errors.scala
+++ src/main/scala/zio/windows/errors.scala
@@ -6,7 +6,7 @@ import java.io.IOException
 
 abstract class WindowsException(msg: String) extends IOException(msg)
 
-case class PlatformException(msg: String) extends LinuxException(msg)
+case class PlatformException(msg: String) extends WindowsException(msg)
 
 case class NativeWindowsException(errorNumber: Int, msg: String) extends LinuxException(msg) {
   def error: Int = errorNumber
