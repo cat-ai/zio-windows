a399c9f3960c9d21ac3c2a37ecce1dd9833c754b
Kemran <45750128+cat-ai@users.noreply.github.com>
2021-02-20 17:09:11 +0300

    Update errors.scala

--- src/main/scala/zio/windows/errors.scala
+++ src/main/scala/zio/windows/errors.scala
@@ -4,15 +4,15 @@ import com.sun.jna.Native
 
 import java.io.IOException
 
-abstract class LinuxException(msg: String) extends IOException(msg)
+abstract class WindowsException(msg: String) extends IOException(msg)
 
 case class PlatformException(msg: String) extends LinuxException(msg)
 
-case class NativeLinuxException(errorNumber: Int, msg: String) extends LinuxException(msg) {
+case class NativeWindowsException(errorNumber: Int, msg: String) extends LinuxException(msg) {
   def error: Int = errorNumber
 }
 
-object NativeLinuxException {
+object NativeWindowsException {
 
   def apply: NativeLinuxException =
     fromError(Native.getLastError)
